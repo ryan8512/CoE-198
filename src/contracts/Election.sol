d381cd30faa058c04f412081181ac512e450d078
Ri-go <46516265+Ri-go@users.noreply.github.com>
2020-05-07 12:56:20 +0800

    New Election Contract
    Supersedes Previous Election

---
+++ src/contracts/Election.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.4.22 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "./Registration.sol";
+import "./SetupBlockchain.sol";
+import "./Ballot.sol";
+contract Election{
+    address ElectionAdmin;
+    uint ElectionDealine;
+    Registration registration;
+    SetupBlockchain setup;
+    mapping(address => address) ballots;
+    
+    constructor() public{
+        ElectionAdmin = msg.sender;
+        ElectionDealine = now + 86400;
+    }
+    
+
+    function vote(string memory _uniqueID, uint256[] memory _votes) public{
+        require(registration.votingStatus(msg.sender) == true, "You're not allowed to vote!");
+        require(registration.authenticateID(msg.sender,_uniqueID) == true, "Invalid Credentials!");
+        Ballot(setup.getBallot(registration.getDistrict(msg.sender))).vote(_votes);
+        registration.hasVoted(msg.sender);
+    }
+    
+    function acquireRegistration(address _address) private{
+        registration = Registration(_address);
+    }
+    function applySetup(address _address) public {
+        setup = SetupBlockchain(_address);
+    }
+} 
