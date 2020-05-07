fa97f6d3bda6b5a0f256f38eccfe97051a713b9b
Ri-go <46516265+Ri-go@users.noreply.github.com>
2020-05-07 12:54:50 +0800

    Added Minting

--- src/contracts/voterToken.sol
+++ src/contracts/voterToken.sol
@@ -6,22 +6,22 @@ contract voterToken{
     string symbol;
     uint256 totalVote;
     mapping (address => uint256) public balance;
-    constructor(string memory _name, string memory _symbol, uint256 _totalVote) public{
+    constructor(string memory _name, string memory _symbol) public{
         owner = msg.sender;
         name = _name;
         symbol = _symbol;
-        totalVote = _totalVote;
-        balance[msg.sender] = _totalVote;
     }
-    
+    function mint(address _to, uint256 _amount) public{
+        require(msg.sender == owner, "You do not have permission to alot voting token");
+        balance[_to] = balance[_to] + _amount;
+        totalVote += _amount;
+    }
     function balanceOf(address _owner) view public returns (uint256){
         return balance[_owner];
     }
     
-    function transfer(address _to, uint _value) public returns (bool){
-        require (balance[msg.sender] >= _value);
-        
-        address _from = msg.sender;
+    function transfer(address _from,address _to, uint _value) public returns (bool){
+        require (balance[_from] >= _value);
         owner = _to;
         balance[_from] = balance[_from] - _value;
         balance[_to] = balance[_to] + _value;
