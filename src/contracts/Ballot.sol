b683c9106a14fe576cc9b9bb64e495c5bb8fc1a1
Ri-go <46516265+Ri-go@users.noreply.github.com>
2020-05-07 12:53:35 +0800

    Updated Ballot.sol

--- src/contracts/Ballot.sol
+++ src/contracts/Ballot.sol
@@ -1,51 +1,49 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity >=0.4.22 <0.7.0;
 pragma experimental ABIEncoderV2;
-
+import "./Registration.sol";
+import "./SetupBlockchain.sol";
 import "./voterToken.sol";
 contract Ballot{
-    string location;
-    voterToken[] token;
-    
+    SetupBlockchain setup;
+    string district;
+    candidate[] Candidates;
+    mapping(uint16 => address) votingTokens;
+    // struct votingToken{
+    //     string name;
+    //     address tokenAddress;
+    //     uint256 amountPerVoter;
+    //     uint16 position;
+    // }
     struct candidate{
         string name;
-        address walletAddress;
-        uint256 position;
+        address candidateAddress;
+        uint16 position;
     }
-    candidate[] public candidatelist;
     
-    constructor(string memory _location,
-    string[] memory _name,
-    address[] memory _walletAddress, 
-    uint256[] memory _position)public{
-        location = _location;
-        for(uint i = 0;i < _name.length;i++){
-            candidatelist.push(candidate({
-               name: _name[i],
-               walletAddress: _walletAddress[i],
-               position: _position[i]
+    constructor(SetupBlockchain.candidate[] memory _candidates,
+    SetupBlockchain.votingToken[] memory _tokens
+    ,string memory _district) public{
+    
+        setup = SetupBlockchain(msg.sender);
+        district = _district;
+        for (uint i =0; i < _candidates.length;i++){
+            Candidates.push(candidate({
+                name: _candidates[i].name,
+                candidateAddress: _candidates[i].candidateAddress,
+                position: _candidates[i].position
             }));
         }
-    }
-    
-    function vote(address[] memory _walletAddress,uint256[] memory _position) public{
-        for(uint i =0; i < _walletAddress.length;i++){
-                if(_position[i] == 1){
-                    token[1].transfer(_walletAddress[i],1);
-                }
-                else if(_position[i] == 2){
-                    token[2].transfer(_walletAddress[i],1);
-                }
-                else{
-                    token[3].transfer(_walletAddress[i],1);
-                }
+        for (uint16 i =0; i < _tokens.length;i++){
+            uint16 j = _tokens[i].position;
+            votingTokens[j] = _tokens[i].tokenAddress;
         }
     }
-    /**************************************DEBUG**********************************************/
-    function listCandidates()public view returns(string[] memory){
-        string[] memory _candidates = new string[](10);
-        for(uint i = 0;i < candidatelist.length;i++)
-            _candidates[i] = candidatelist[i].name;
-        return _candidates;
+    function vote(uint256[] memory votes, address _address) public returns(uint x){
+        for (uint i = 0;i < votes.length;i++){
+            voterToken(votingTokens[Candidates[votes[0]].position]).transfer(_address,Candidates[votes[0]].candidateAddress,1);
+        }
     }
-    
+    function getCandidates() public view returns(candidate[] memory _candidateList){
+        return Candidates;
+    }
 }
